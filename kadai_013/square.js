6a444eeec5bd10fb4afb66098f82e13aca2484e5
rysk-17 <136770680+rysk-17@users.noreply.github.com>
2023-08-25 10:29:33 +0900

    Add files via upload

---
+++ kadai_013/square.js
@@ -0,0 +1,9 @@
+// const square = (value) =>{
+//     return value * value;
+// }
+// function square(value){
+//     return value * value;
+// }
+const square = value => value * value
+console.log(square(2));
+
